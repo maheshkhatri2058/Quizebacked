c9e8d19fe21e0c07185051eba28f81cc6ce5199d
Mahesh <maheshkhatri2058@gmail.com>
2025-07-19 18:44:54 +0530

    new files updated

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3008;
+const BASE_URL = process.env.URL;
+const cors = require('cors');
+mongoose.connect(BASE_URL).then(()=>console.log("connected")).catch((err)=>console.log("errors r there",err));
+const users=mongoose.Schema({
+    Username:String,
+    AUID:String,
+    Score:Number
+
+});
+const user=mongoose.model('users',users);
+
+app.post('/sendinfo', async(req, res) => {
+    const data=req.body;
+    const usr = new user(data);
+    try {
+        await usr.save();
+        res.send("data saved");
+        console.log(usr);
+        } catch (err) {
+            res.send("error");
+            }
+ 
+});
+app.get('/getinfo',async(req,res)=>
+{
+    const data=await user.find();
+    res.send(data);
+    console.log(data);
+
+
+});
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
